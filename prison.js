95ef363094740f365110192ce4bf89eb1b87b8bf
CharlotEden <75186765+CharlotEden@users.noreply.github.com>
2022-10-18 15:42:22 -0400

    Update prison.js

--- prison.js
+++ prison.js
@@ -1,4 +1,26 @@
-let num = 10
+let num = 100
+let maxOpened = num/2
+
+let mode = "text"
+
+function narrate(text) {
+    if(mode === "text"){
+        narrate = console.log
+    }
+}
+
+function silence() {
+    narrate = function() {}
+}
+
+function unsilence() {
+    narrate = function (text) {
+        if(mode === "text"){
+            narrate = console.log
+        }
+    }
+}
+
 
 // get random number 
 function getRandom(min, max) {
@@ -26,22 +48,22 @@ function shuffle(array) {
 
 
 
-let prisonners = []
+let prisoners = []
 
 let names = []
 // set names to make into objects
 for (let i = 0; i < num; i++) {
-    names.push("prisonner"+i)
+    names.push("prisoner"+i)
 }
 
 // turn names into objects and give name id, opened, found
 for (let i = 0; i < names.length; i++) {    
     names[i] = new Object();
-    names[i].name = "prisonner"+(i+1)
+    names[i].name = "prisoner"+(i+1)
     names[i].id = i+1
     names[i].opened = 0
     names[i].found = false
-    prisonners.push(names[i])
+    prisoners.push(names[i])
 }
 
 let boxNames = []
@@ -79,10 +101,144 @@ for (let i = 0; i < boxes.length; i++) {
 
 // =============================================
 
-// did prisonner find their number
-function checkIfFound(prisonner, box){
-    if (prisonner.id === box.content) {
-        return true
-    } else{return false}
+function closeAllBoxes() {
+    for (let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
+        box.opened = false
+    }
 }
+
+let results = []
+
+// method 1: look through first half boxes
+function method1(){
+
+    let foundIds = 0
+
+    for (let i = 0; i < prisoners.length; i++) {
+        const prisoner = prisoners[i];
+
+        narrate(">> It's ", prisoner.name, "'s turn.")
+        
+        
+        let looking = true
+        
+        while (looking) {
+            for (let o = 0; o < boxes.length; o++) {
+                const box = boxes[o];
+                box.opened = true
+                prisoner.opened++
+                narrate("     ", box.name,"had", box.content, "inside.")
+                
+                if (box.content === prisoner.id) {
+                    narrate("     ", box.content, "matches the prisoner's id ", prisoner.id)
+                    narrate("     Because of that, moving on to the next prisoner.")
+                    narrate("     ")
+                    narrate("     ")
+                    prisoner.found = true
+                    foundIds++
+                    looking = false
+                    o = num*10
+                    break
+                } else if(prisoner.opened === maxOpened) {
+                    narrate("     ")
+                    narrate("     Reached max boxes opened. Moving on.")
+                    narrate("     ")
+                    looking = false
+                    o = num*10
+                    break
+                }
+            }
+        }
+
+
+        closeAllBoxes()
+
+    }
+
+    narrate("  ===  Total Ids Found ", foundIds, " out of ", num, " prisoners", "  ===  ")
+    results.push(foundIds)
+}
+
+
+
+// method 2: go to the box that the content tells you to. 
+function method2(){
+
+    let foundIds = 0
+
+    for (let i = 0; i < prisoners.length; i++) {
+        const prisoner = prisoners[i];
+
+        narrate(">> It's ", prisoner.name, "'s turn.")
+
+        let firstBox = boxes[getRandom(0,boxes.length)]
+
+        firstBox.opened = true
+        prisoner.opened++
+        narrate("     ", firstBox.name,"had", firstBox.content, "inside.")
+
+        let target = firstBox.content
+        for (let o = 0; o < maxOpened; o++) {
+
+            let box = boxes[target-1]
+
+            box.opened = true
+            prisoner.opened++
+            narrate("     ", box.name,"had", box.content, "inside.")
+
+            if (box.content === prisoner.id) {
+                narrate("     ", box.content, "matches the prisoner's id ", prisoner.id)
+                narrate("     Because of that, moving on to the next prisoner.")
+                narrate("     ")
+                narrate("     ")
+                prisoner.found = true
+                foundIds++
+                o = num*10
+                break
+            } else if(prisoner.opened === maxOpened) {
+                narrate("     ")
+                narrate("     Reached max boxes opened. Moving on.")
+                narrate("     ")
+                o = num*10
+                break
+            }
+
+            target = box.content
+            
+        }
+
+        closeAllBoxes()
+
+    }
+
+    narrate("  ===  Total Ids Found ", foundIds, " out of ", num, " prisoners", "  ===  ")
+    results.push(foundIds)
+}
+
+method2()
+
+
+function findTheAverage(array){
+    let sum = 0
+    for (let index = 0; index < array.length; index++) {
+        const x = array[index];
+        sum+=x
+    }
+    sum/=array.length
+    return sum
+}
+
+
+let runtime = 10000
+
+for (let i = 0; i < runtime; i++) {
+    silence()
+    method2()
+}
+unsilence()
+narrate("     ")
+narrate("     ")
+narrate("  ===  Average IDs found", findTheAverage(results), "  ===  ")
+
 
